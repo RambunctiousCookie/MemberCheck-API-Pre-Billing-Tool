d78e968a0c7f7dbda1a6f312198962a38d3e6cba
RambunctiousCookie <jason.tay.nw@gmail.com>
2023-11-01 05:43:02 +0800

    Unit tests

--- src/test/java/Util/TreeUtilTest.java
+++ src/test/java/Util/TreeUtilTest.java
@@ -4,33 +4,24 @@ import Service.ApiService;
 import data.TreeNode;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 
 public class TreeUtilTest {
 
-    private static ApiService apiService;
-
-    private static CalculationUtil calculationUtil;
+    private static TreeNode[] treeNodes;
+    private static TreeNode root;
 
     private static LocalDate[] desiredDate;
 
-    private static TreeNode[] treeNodes;
-
-    private static TreeNode root;
-
     @BeforeAll
     public static void setup() {
-        apiService = Mockito.mock(ApiService.class);
-        calculationUtil = Mockito.mock(CalculationUtil.class);
-        desiredDate = new LocalDate[] {
-                LocalDate.of(2023, 1, 1),
-                LocalDate.of(2023, 12, 31)
-        };
 
         treeNodes = new TreeNode[]{
                 new TreeNode("Root_Org", "Root"),
@@ -45,33 +36,40 @@ public class TreeUtilTest {
         root.getChildren().add(treeNodes[2]);
         root.getChildren().get(0).getChildren().add(treeNodes[3]);
         root.getChildren().get(0).getChildren().add(treeNodes[4]);
+
+        desiredDate = DateUtil.getQuartileDates(2023,4);
     }
 
     @Test
     public void testSumSumSingleAndBatchScansForPeriodReturnsCorrectSum() throws IOException {
 
-        int expectedSum = 0;
+        try(MockedStatic<CalculationUtil> calculationUtil = Mockito.mockStatic(CalculationUtil.class)){
+            int expectedSum = 0;
 
-        for(int i=0;i< treeNodes.length;i++){
-            Mockito.when(calculationUtil.getTotalScansByOrgId(apiService, treeNodes[i].getId(), desiredDate)).thenReturn(i);
-            expectedSum+=i;
+            for(int i=0;i< treeNodes.length;i++){
+                int finalI = i;
+                calculationUtil.when(() -> CalculationUtil.getTotalScansByOrgId(Mockito.any(), eq(treeNodes[finalI].getId()), Mockito.any())).thenReturn(i);
+                expectedSum+=i;
+            }
+
+            int actualSum = TreeUtil.sumSumSingleAndBatchScansForPeriod(root, Mockito.mock(ApiService.class), desiredDate);
+
+            assertEquals(expectedSum, actualSum);
         }
-
-        int actualSum = TreeUtil.sumSumSingleAndBatchScansForPeriod(root, apiService, desiredDate);
-
-        assertEquals(expectedSum, actualSum);
     }
 
     @Test
     public void testSumSumSingleAndBatchScansForPeriodIteratesThroughEveryNode() throws IOException {
-        Mockito.when(CalculationUtil.getTotalScansByOrgId(apiService, Mockito.any(), desiredDate)).thenReturn(1);
-
         int expectedCount = treeNodes.length;
 
-        int actualCount = TreeUtil.sumSumSingleAndBatchScansForPeriod(root, apiService, desiredDate);
+        try(MockedStatic<CalculationUtil> calculationUtil = Mockito.mockStatic(CalculationUtil.class)){
+            calculationUtil.when(() -> CalculationUtil.getTotalScansByOrgId(Mockito.any(), Mockito.any(), Mockito.any()))
+                    .thenReturn(1);
 
-        assertEquals(expectedCount, actualCount);
+            int actualCount = TreeUtil.sumSumSingleAndBatchScansForPeriod(root, Mockito.mock(ApiService.class), desiredDate);
 
+            assertEquals(expectedCount, actualCount);
+        }
     }
 
 }
